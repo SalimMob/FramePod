ec96fa609cef3c9b4fc001c90107939e0f6d4e17
Salim Maalouf <salim.maalouf@mymonty.com>
2024-12-16 13:36:07 +0200

    first commit

---
+++ FramePod/FramePod.h
@@ -0,0 +1,18 @@
+//
+//  FramePod.h
+//  FramePod
+//
+//  Created by Salim Maalouf on 16/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FramePod.
+FOUNDATION_EXPORT double FramePodVersionNumber;
+
+//! Project version string for FramePod.
+FOUNDATION_EXPORT const unsigned char FramePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FramePod/PublicHeader.h>
+
+
